573ccf9e7779b9c5fe84d5de594df464490e115a
Shuaiyi Wu <wushuaiyi@outlook.com>
2016-06-27 23:37:35 +0800

    Update Avocado.ps1

--- Avocado.ps1
+++ Avocado.ps1
@@ -1,5 +1,11 @@
+param (
+    [parameter(mandatory=$true)]
+    [hashtable]$ParameterDictionary
+)
+
 write-host "I like Avocado with milk mix"
-write-host "Today is Jun 24th"
-write-host "help yourself !"
 
 $ParameterDictionary
+
+write-host "Today is Jun 24th"
+write-host "help yourself !"
