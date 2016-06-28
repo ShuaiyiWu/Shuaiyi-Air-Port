6bd66ad8034dc8beabee62103a82cfb136af9b77
Shuaiyi Wu <wushuaiyi@outlook.com>
2016-06-28 16:00:29 +0800

    Update Avocado.ps1

--- Avocado.ps1
+++ Avocado.ps1
@@ -4,8 +4,8 @@ param (
 )
 
 write-host "I like Avocado with milk mix"
-
-write-host $ParameterDictionary
-
 write-host "Today is Jun 24th"
 write-host "help yourself !"
+
+$temp = $ParameterDictionary.environment.repositoryRoot
+write-host $temp
