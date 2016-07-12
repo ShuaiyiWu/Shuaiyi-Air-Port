4f632494da565f8bda09ec3ded279f6e74894831
Shuaiyi Wu <wushuaiyi@outlook.com>
2016-07-12 10:03:08 +0800

    Update Avocado.ps1

--- Avocado.ps1
+++ Avocado.ps1
@@ -3,7 +3,13 @@ param (
     [hashtable]$ParameterDictionary
 )
 
+# Main
+$source = $($MyInvocation.MyCommand.Definition)
+$reportFilePath = $ParameterDictionary.environment.logFile
+
 write-host "I like Avocado with milk mix"
 write-host "Test url download successfully"
+
+LogWarning -logFilePath $reportFilePath -message "Log warning test" -source $source -line $Myinvocation.ScriptlineNumber
 
 exit $LASTEXITCODE
